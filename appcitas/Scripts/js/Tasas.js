147de884ed077f3784e010046d44a28a43963328
rromerosyt <49084373+rromerosyt@users.noreply.github.com>
2019-04-03 09:21:16 -0600

    correccion registro de mas en resultado
    eliminar el elemento 0 del combo que es Seleccionar Resultado

--- appcitas/Scripts/js/Tasas.js
+++ appcitas/Scripts/js/Tasas.js
@@ -242,7 +242,9 @@ function ObtenerArrayDeResultados(formulario='') {
         return option.value;
     });
 
-    for (var i = 0; i < options.length; i++) {
+    //for (var i = 0; i < options.length; i++) {
+    // se pone desde 1 porque el item 0 es "Seleccione un Resultado"
+    for (var i = 1; i < options.length; i++) {
         if ($('#ResultadosCbx' + formulario).val() === values[i]) {
             resultadoArray.push({
                 ResultadoReversionId: values[i],
